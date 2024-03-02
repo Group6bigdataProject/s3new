8dbc452c1545e8425f9a74a814c7f81a75bfc9c1
JadhavManisha12 <manishajadhavjob@gmail.com>
2024-03-02 18:31:35 +0530

    project

--- injesion.py
+++ injesion.py
@@ -5,6 +5,7 @@ from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
 ## @params: [JOB_NAME]
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
@@ -17,28 +18,41 @@ glueContext = GlueContext(spark.sparkContext)
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Read data from S3
-df3 = spark.read.csv("s3://gpp6/newstreet/", header=True)
+# Define schema for the CSV file
+schema = StructType([
+    StructField("Crime ID", StringType(), True),
+    StructField("Month", StringType(), True),
+    StructField("Reported by", StringType(), True),
+    StructField("Falls within", StringType(), True),
+    StructField("Longitude", DoubleType(), True),
+    StructField("Latitude", DoubleType(), True),
+    StructField("Location", StringType(), True),
+    StructField("LSOA code", StringType(), True),
+    StructField("LSOA name", StringType(), True),
+    StructField("Crime type", StringType(), True),
+    StructField("Last outcome category", StringType(), True),
+    StructField("Context", StringType(), True)
+])
+
+# Read data from S3 with specified schema
+df3 = spark.read.csv("s3://gpp6/newstreet/", header=True, schema=schema)
 
 # Rename columns
-df4 = df3.withColumnRenamed("Crime ID","CrimeID") \
-         .withColumnRenamed("Month","MONTH") \
-         .withColumnRenamed("Reported by","ReportedBy") \
-         .withColumnRenamed("Crime type","CrimeType") \
-         .withColumnRenamed("Falls within","FallsWithin") \
-         .withColumnRenamed("Longitude","Longitude") \
-         .withColumnRenamed("Last outcome category","LastOutcomeCategory") \
-         .withColumnRenamed("Context","Contexts") \
-         .withColumnRenamed("Latitude","Latitude") \
-         .withColumnRenamed("Location","Locations") \
-         .withColumnRenamed("LSOA code","LSOACODE") \
-         .withColumnRenamed("LSOA name","LSOANAME")
+df4 = df3.withColumnRenamed("Crime ID", "CrimeID") \
+    .withColumnRenamed("Month", "MONTH") \
+    .withColumnRenamed("Reported by", "ReportedBy") \
+    .withColumnRenamed("Crime type", "CrimeType") \
+    .withColumnRenamed("Falls within", "FallsWithin") \
+    .withColumnRenamed("Longitude", "Longitude") \
+    .withColumnRenamed("Last outcome category", "LastOutcomeCategory") \
+    .withColumnRenamed("Context", "Contexts") \
+    .withColumnRenamed("Latitude", "Latitude") \
+    .withColumnRenamed("Location", "Locations") \
+    .withColumnRenamed("LSOA code", "LSOACODE") \
+    .withColumnRenamed("LSOA name", "LSOANAME")
 
 # Write to Parquet
-
-
 df4.coalesce(1).write.parquet("s3://group-6-datalakenew23re6dlrefyuti/street/", mode="append")
- 
 
 # Fetching data from RDS
 # Read data from RDS using Glue Catalog
@@ -50,21 +64,20 @@ rds_data_source = glueContext.create_dynamic_frame.from_catalog(database=rds_dat
 rds_df = rds_data_source.toDF()
 
 # Rename columns
-df2 = rds_df.withColumnRenamed("Crime ID","CrimeID") \
-         .withColumnRenamed("Month","MONTH") \
-         .withColumnRenamed("Reported by","ReportedBy") \
-         .withColumnRenamed("Falls within","FallsWithin") \
-         .withColumnRenamed("Longitude","Longitude") \
-         .withColumnRenamed("Latitude","Latitude") \
-         .withColumnRenamed("Location","Locations") \
-         .withColumnRenamed("LSOA code","LSOACODE") \
-         .withColumnRenamed("LSOA name","LSOANAME") \
-         .withColumnRenamed("Outcome type","Outcometype") \
-         .withColumnRenamed("crime type","crimetype") \
-         .withColumnRenamed("last outcome category","lastoutcomecategory")
+df2 = rds_df.withColumnRenamed("Crime ID", "CrimeID") \
+    .withColumnRenamed("Month", "MONTH") \
+    .withColumnRenamed("Reported by", "ReportedBy") \
+    .withColumnRenamed("Falls within", "FallsWithin") \
+    .withColumnRenamed("Longitude", "Longitude") \
+    .withColumnRenamed("Latitude", "Latitude") \
+    .withColumnRenamed("Location", "Locations") \
+    .withColumnRenamed("LSOA code", "LSOACODE") \
+    .withColumnRenamed("LSOA name", "LSOANAME") \
+    .withColumnRenamed("Outcome type", "Outcometype") \
+    .withColumnRenamed("crime type", "crimetype") \
+    .withColumnRenamed("last outcome category", "lastoutcomecategory")
 
 # Write to Parquet
-
 df2.repartition(90).write.parquet("s3://group-6-datalakenew23re6dlrefyuti/outcomes/", mode="append")
 
 # Stop SparkSession
