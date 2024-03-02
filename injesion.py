d87115d6f24505ed0231de65839616dcd14be88d
JadhavManisha12 <manishajadhavjob@gmail.com>
2024-03-02 21:26:48 +0530

    project1

--- injesion.py
+++ injesion.py
@@ -52,7 +52,7 @@ df4 = df3.withColumnRenamed("Crime ID", "CrimeID") \
     .withColumnRenamed("LSOA name", "LSOANAME")
 
 # Write to Parquet
-df4.coalesce(1).write.parquet("s3://group-6-datalakenew23re6dlrefyutilast/street/", mode="append")
+df4.coalesce(1).write.parquet("s3://group56datalake44/street/", mode="append")
 
 # Fetching data from RDS
 # Read data from RDS using Glue Catalog
@@ -78,7 +78,7 @@ df2 = rds_df.withColumnRenamed("Crime ID", "CrimeID") \
     .withColumnRenamed("last outcome category", "lastoutcomecategory")
 
 # Write to Parquet
-df2.repartition(90).write.parquet("s3://group-6-datalakenew23re6dlrefyutilast/outcomes/", mode="append")
+df2.repartition(90).write.parquet("s3://group56datalake44/outcomes/", mode="append")
 
 # Stop SparkSession
 spark.stop()
